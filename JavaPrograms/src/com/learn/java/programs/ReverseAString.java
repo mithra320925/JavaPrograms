d847771d231de69531bfa0736faef076e6a96c71
Mithr <Mithr@DESKTOP-22DAPIM>
2021-03-08 16:28:27 +0530

    commit

---
+++ JavaPrograms/src/com/learn/java/programs/ReverseAString.java
@@ -0,0 +1,16 @@
+package com.learn.java.programs;
+
+public class ReverseAString {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String given="Agni";
+		
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(given);
+		System.out.println(buffer.reverse());
+
+	}
+
+}
